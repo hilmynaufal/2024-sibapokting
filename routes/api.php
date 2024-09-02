a521619f803d76ad4a1c2abe3d52eb5d23949db0
Didik Kurniawan - Macbook <didikkurniawannn@gmail.com>
2024-09-02 13:26:25 +0700

    bug : menambahkan route API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChartController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RefController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SuratMasukController;
+use App\Http\Controllers\DisposisiCreateController;
+use App\Http\Controllers\DisposisiListController;
+use App\Http\Controllers\LampiranController;
+use App\Models\DisposisiLaporan;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,3 +23,43 @@
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('dataChart', [ChartController::class,'dataChart']);
+Route::get('komoditasBar', [ChartController::class,'komoditasBar']);
+Route::get('komoditasLine', [ChartController::class,'komoditasLine']);
+Route::post('tabelKomoditas', [ChartController::class,'chartPasarStatistik']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Auth
+Route::post('Login', [UserController::class,'login']);
+// Route::post('auth/login-sso', [AuthController::class,'auth']);
+
+Route::post('auth/callback', [AuthController::class,'auth']);
+Route::get('TestApi', [RefController::class,'Test']);
+
+// Master
+Route::get('GetJenisDisposisi', [RefController::class,'getJenisDisposisi']);
+Route::get('GetJenisSurat', [RefController::class,'getJenisSurat']);
+Route::get('GetSifatSurat', [RefController::class,'getSifatSurat']);
+Route::get('GetPegawai', [RefController::class,'getPegawai']);
+Route::get('GetUnitKerja', [RefController::class,'getUnitKerja']);
+
+// Surat Masuk
+Route::post('InsertSuratMasuk', [SuratMasukController::class,'create']);
+Route::post('ListSuratMasuk', [SuratMasukController::class,'index']);
+Route::post('TrackingVerifikasi', [SuratMasukController::class,'tracking']);
+
+// Disposisi Masuk
+Route::post('InsertDisposisi', [DisposisiCreateController::class,'create']);
+Route::post('TrackingDisposisi', [DisposisiListController::class,'tracking']);
+Route::post('ListDisposisi', [DisposisiListController::class,'index']);
+Route::post('LaporanDisposisi', [DisposisiLaporan::class,'create']);
+
+// lampiran
+Route::post('UploadLampiran', [LampiranController::class,'uploadLampiran']);
+
+// Dashboard
+Route::post('Dashboard', [DashboardController::class,'index']);
