c5fe47188394c5bc90a624fc1ab65bd2528a3275
Didik Kurniawan - Macbook <didikkurniawannn@gmail.com>
2024-12-09 01:58:10 +0700

    bug : perbaiki niddleware

--- app/Http/Middleware/FrameHeadersMiddlewar.php
+++ app/Http/Middleware/FrameHeadersMiddlewar.php
@@ -18,7 +18,10 @@ class FrameHeadersMiddlewar
     {
         // Tambahkan logika middleware Anda di sini
         $response = $next($request);
-        $response->header('X-Frame-Options', 'ALLOW-FROM https://dashboardbedas.bandungkab.go.id');
+        $response->headers->remove('X-Frame-Options');  // Hapus header jika tidak perlu
+        // Atau ubah menjadi 'allow-from' jika perlu
+        $response->headers->set('X-Frame-Options', 'ALLOWALL');  // Atau 'SAMEORIGIN' jika diperlukan
+        $response->headers->set('Content-Security-Policy', "frame-ancestors *;");
         return $response;
     }
 }
